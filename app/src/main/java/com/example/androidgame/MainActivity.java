7d20f5670d2a572cc735b665c68a3b9cb7dbe35b
Ad04a <andrey.todorov04@gmail.com>
2020-11-08 14:17:58 +0200

    nachalo na naimenovane

--- app/src/main/java/com/example/androidgame/MainActivity.java
+++ app/src/main/java/com/example/androidgame/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.androidgame;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.solver.widgets.WidgetContainer;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -34,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
         //WORKER HOUSE
         TextView house_wdgt =(TextView) findViewById(R.id.house_wdgt);
+        ImageButton workerBtn1=(ImageButton) findViewById(R.id.workerBtn1);
+        ImageButton workerBtn2=(ImageButton) findViewById(R.id.workerBtn2);
+        ImageButton[] house_btns = {workerBtn1,workerBtn2};
         TextView[] house_Wdgts = {house_wdgt};
 
 
@@ -44,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                     set_butt_vis_on(menu_btns);
                     set_wdgt_vis_on(menu_Wdgts);
                     set_wdgt_vis_off(house_Wdgts);
+                    set_imgbutt_vis_off(house_btns);
                 }else{
                     set_butt_vis_off(menu_btns);
                     set_wdgt_vis_off(menu_Wdgts);
@@ -60,8 +66,10 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (!house_wdgt.isShown()) {
                     set_wdgt_vis_on(house_Wdgts);
+                    set_imgbutt_vis_on(house_btns);
                 } else {
                     set_wdgt_vis_off(house_Wdgts);
+                    set_imgbutt_vis_off(house_btns);
                 }
             }
         });
@@ -86,5 +94,16 @@ public class MainActivity extends AppCompatActivity {
             next.setVisibility(View.GONE);
         }
     }
+    private void set_imgbutt_vis_on(ImageButton[] args){
+        for(ImageButton next:args){
+            next.setVisibility(View.VISIBLE);
+        }
+    }
+    private void set_imgbutt_vis_off(ImageButton[] args){
+        for(ImageButton next:args){
+            next.setVisibility(View.GONE);
+        }
+    }
+
 }
 
