da02efee50b5dc278cee4b924a1f3ee84ff5109b
RayaLitova <raya.litova23@gmail.com>
2020-11-08 02:20:30 +0200

    added menu

--- app/src/main/java/com/example/androidgame/MainActivity.java
+++ app/src/main/java/com/example/androidgame/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.androidgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.WidgetContainer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button menu_btn=(Button) findViewById(R.id.menu);
+        Button shop_btn=(Button) findViewById(R.id.shop_btn);
+        Button store_btn=(Button) findViewById(R.id.store_btn);
+        Button settings_btn=(Button) findViewById(R.id.settings_btn);
+        TextView menu_wdgt=(TextView) findViewById(R.id.menu_wdgt);
+        menu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!menu_wdgt.isShown()) {
+                    menu_wdgt.setVisibility(View.VISIBLE);
+                    shop_btn.setVisibility(View.VISIBLE);
+                    store_btn.setVisibility(View.VISIBLE);
+                    settings_btn.setVisibility(View.VISIBLE);
+                }else{
+                    menu_wdgt.setVisibility(View.GONE);
+                    shop_btn.setVisibility(View.GONE);
+                    store_btn.setVisibility(View.GONE);
+                    settings_btn.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
